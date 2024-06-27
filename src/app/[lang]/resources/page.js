a531f3e9140bb85de7ed015d4ed4223ab72ad6d3
ma-nloh <loh@ma-n.com>
2024-06-27 10:28:17 +0800

    check fix bug

--- src/app/[lang]/resources/page.js
+++ src/app/[lang]/resources/page.js
@@ -67,7 +67,7 @@ export default async function Page({ params: { lang } }) {
   )
 }
 
-export const ImageBanner = ({ image }) => (
+const ImageBanner = ({ image }) => (
   <Image src={image} className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100 object-cover" alt='image' />
 );
 
