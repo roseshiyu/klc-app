abd4dd62b1ed597b22de4615e1e8999f5f5e3dd3
ma-nloh <loh@ma-n.com>
2024-06-30 16:40:26 +0800

    temp fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,8 +5,8 @@ import { Footer } from "./ui/footer";
 import { Header } from "./ui/header";
 import Image from "next/image";
 import { whatsApp } from "@/public";
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 
 const inter = Noto_Sans_SC({ subsets: ["latin"] });
 
@@ -23,28 +23,33 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({
-  children,
+  // children,
   params: { locale },
 }: Readonly<{
-  children: React.ReactNode;
+  // children: React.ReactNode;
   params: { locale: string }
 }>) {
   const messages = await getMessages();
-  
+ 
   return (
-    <html lang={locale}>
-      <body className={inter.className}>
-        <NextIntlClientProvider messages={messages}>
-          <div>
-            <Header />
-            {children}
-            <Footer />
-            <a href="https://wa.me/01111478477" target="_blank">
-              <Image className="fixed bottom-5 right-5 rounded-md" alt="Chat on WhatsApp" src={whatsApp} />
-            </a>
-          </div>
-        </NextIntlClientProvider>
+    <html>
+      <body>
+        <div>under maintaince</div>
       </body>
     </html>
+    // <html lang={locale}>
+    //   <body className={inter.className}>
+    //     <NextIntlClientProvider messages={messages}>
+    //       <div>
+    //         <Header />
+    //         {children}
+    //         <Footer />
+    //         <a href="https://wa.me/01111478477" target="_blank">
+    //           <Image className="fixed bottom-5 right-5 rounded-md" alt="Chat on WhatsApp" src={whatsApp} />
+    //         </a>
+    //       </div>
+    //     </NextIntlClientProvider>
+    //   </body>
+    // </html>
   );
 }
--- src/app/[locale]/sitemap.ts
+++ src/app/[locale]/sitemap.ts
@@ -1,22 +1,22 @@
-import { MetadataRoute } from 'next'
+// import { MetadataRoute } from 'next'
  
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://klc-app.vercel.app',
-      lastModified: new Date(),
-    },
-    // {
-    //   url: 'https://acme.com/about',
-    //   lastModified: new Date(),
-    //   changeFrequency: 'monthly',
-    //   priority: 0.8,
-    // },
-    // {
-    //   url: 'https://acme.com/blog',
-    //   lastModified: new Date(),
-    //   changeFrequency: 'weekly',
-    //   priority: 0.5,
-    // },
-  ]
-}
+// export default function sitemap(): MetadataRoute.Sitemap {
+//   return [
+//     {
+//       url: 'https://klc-app.vercel.app',
+//       lastModified: new Date(),
+//     },
+//     // {
+//     //   url: 'https://acme.com/about',
+//     //   lastModified: new Date(),
+//     //   changeFrequency: 'monthly',
+//     //   priority: 0.8,
+//     // },
+//     // {
+//     //   url: 'https://acme.com/blog',
+//     //   lastModified: new Date(),
+//     //   changeFrequency: 'weekly',
+//     //   priority: 0.5,
+//     // },
+//   ]
+// }
