87aec1e2d5999e865148e661c6c99680d0363cee
Loh <kian.xing@judauniverse.com>
2024-11-15 14:25:03 +0800

    lang

--- middleware.js
+++ middleware.js
@@ -2,13 +2,14 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['cn', 'en'],
-
+  // locales: ['cn', 'en'],
+  locales: ['cn'],
   // Used when no locale matches
   defaultLocale: 'cn'
 });
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(cn|en)/:path*']
+  // matcher: ['/', '/(cn|en)/:path*']
+  matcher: ['/', '/(cn)/:path*']
 };
